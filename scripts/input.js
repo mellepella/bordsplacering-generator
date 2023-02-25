8ed3f328f9f451d26a7a01d2c037015f3cbf3894
Melvin Wahlberg <43107226+mellepella@users.noreply.github.com>
2023-02-25 14:01:09 +0100

    Update input.js

--- scripts/input.js
+++ scripts/input.js
@@ -1,4 +1,4 @@
-const input =
-  "Ann-Marie Bertil Camilla Peter Miriam Mattias Peter Annette Stina Susanne Jimmie Jonathan Henrik Maria Isolde Robin Tilde Emilio Anton Jens Jennie Margaretha Alf Sophia Mats Ellen Emma Jocke Emric Emmy Melvin Tuva Emil Maria Ernst Bernard Evelina Jacob Lovelia".split(
+const input = [
+  ..."Ann-Marie Bertil Camilla Miriam Mattias Annette Stina Susanne Jimmie Jonathan Henrik Maria Isolde Robin Tilde Emilio Anton Jens Jennie Margaretha Alf Sophia Mats Ellen Emma Jocke Emric Emmy Melvin Tuva Emil Maria Ernst Bernard Evelina Jacob Lovelia".split(
     " "
-  );
+  ), "Peter L", "Peter N"];
